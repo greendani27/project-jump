7242e5fb023db512424a07a4911ebfb478f5b0d0
greendani27 <94862098+greendani27@users.noreply.github.com>
2024-04-13 19:17:49 +0200

    Add interfaz para reutilizar codigo
    Añadida la interfaz ICharacter,  para todos los personajes que se muevan y peguen

---
+++ Assets/Scripts/ICharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacter
+{
+    public void Move();
+    public void Punch();
+    public void Flip();
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, ICharacter
 {
+    // Podria sacar una interfaz (ICharacter por ejemplo) con los metodos move, punch y flip para tanto el player como los enemies
+
     [SerializeField] Transform leftFootCheckPoint;
     [SerializeField] Transform rightFootCheckPoint;
+    [SerializeField] Transform attackPoint;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask enemyLayer;
     [SerializeField] Rigidbody2D rbb;
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer sr;
@@ -14,10 +18,10 @@ public class PlayerController : MonoBehaviour
     private float horizontalAxis;
 
     public float jumpForce;
+    public float attackRange;
 
     public float minSpeed;
     public float maxSpeed;
-
     private float speed;
 
     private bool onGround;
@@ -26,8 +30,6 @@ public class PlayerController : MonoBehaviour
     {
         horizontalAxis = Input.GetAxis("Horizontal");
 
-        Move();
-
         onGround = Physics2D.OverlapCircle(leftFootCheckPoint.position, .1f, groundLayer) || Physics2D.OverlapCircle(rightFootCheckPoint.position, .1f, groundLayer);
 
         if (onGround == true)
@@ -35,40 +37,22 @@ public class PlayerController : MonoBehaviour
         else
             animator.SetBool("onGround", false);
 
+        Move();
         Jump();
-
         Punch();
     }
 
-    private void Move()
-    {
-        if (horizontalAxis != 0)
+    private void Sprint() {
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            animator.SetBool("Running", true);
-
-            if (horizontalAxis < 0)
-            {
-                sr.flipX = true;
-            }
-            else {
-                sr.flipX = false;
-            }
-
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                speed = maxSpeed;
-                animator.speed = 2;
-            }
-            else
-            {
-                speed = minSpeed;
-                animator.speed = 1;
-            }
-
-            rbb.velocity = new Vector2(horizontalAxis * speed, GetComponent<Rigidbody2D>().velocity.y);
+            speed = maxSpeed;
+            animator.speed = 2;
         }
         else
-            animator.SetBool("Running", false);
+        {
+            speed = minSpeed;
+            animator.speed = 1;
+        }
     }
 
     private void Jump()
@@ -83,16 +67,49 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Punch() {
-        if (Input.GetMouseButtonDown(0)) {
-            animator.SetTrigger("Punch");
+    public void Move()
+    {
+        if (horizontalAxis != 0)
+        {
+            animator.SetBool("Running", true);
+
+            Sprint();
+            Flip();
+
+            rbb.velocity = new Vector2(horizontalAxis * speed, GetComponent<Rigidbody2D>().velocity.y);
         }
-        
+        else
+            animator.SetBool("Running", false);
     }
 
-    /*private void OnDrawGizmosSelected()
+    public void Punch()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Punch");
+            if (Physics2D.OverlapCircle(attackPoint.position, attackRange, enemyLayer))
+            {
+                Debug.Log("entro");
+            }
+        }
+    }
+
+    public void Flip()
+    {
+        if (horizontalAxis < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(leftFootCheckPoint.position, .1f);
         Gizmos.DrawWireSphere(rightFootCheckPoint.position, .1f);
-    }*/
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
